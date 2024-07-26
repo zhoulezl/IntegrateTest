706d466008343e2cb0ed54c256eca749926fb8bf
liuyu3053 <790746421@qq.com>
2024-07-26 11:27:32 +0800

    liuyu
    增加删除指定文件方法
    修复由于运行时网络环境不稳定导致的登录失败等从而使测试失败的问题

--- Auto_test_ly/play_UI/do_some.py
+++ Auto_test_ly/play_UI/do_some.py
@@ -1,4 +1,5 @@
-import assert_dict
+import time
+
 import operation_dict
 from playwright.sync_api import sync_playwright
 import case_list
@@ -27,16 +28,20 @@ if __name__ == '__main__':
 
     case_list = case_list.get_case_list(file_path)
     for case in case_list:
+        print(case)
         page = context.new_page()
         def_names = case[1]
         for def_name in def_names:
-
+            print(' ', def_name)
             try:
-                if type(def_name) == str:
+                if type(def_name) is str:
                     call_function_by_name(operation_dict.def_dict[def_name].__name__, page)
-                elif type(def_name) == list:
+                elif type(def_name) is list:
                     call_function_by_name(operation_dict.def_dict[def_name[0]].__name__, page, def_name[1].split(','))
             except Exception as e:
                 print(e)
+            finally:
+                time.sleep(2)
+        page.close()
     # 结束跟踪
     context.tracing.stop(path="trace.zip")
--- Auto_test_ly/play_UI/operation_dict.py
+++ Auto_test_ly/play_UI/operation_dict.py
@@ -1,8 +1,6 @@
 import inspect
-import re
 import time
 import pyautogui
-import playwright
 from playwright import sync_api
 from playwright.sync_api import expect
 
@@ -24,54 +22,53 @@ def tag_list_operation(arglist):
 
 
 def login_sh(page: sync_api.Page):
-    screenshot_count = 1
-    name = inspect.currentframe().f_code.co_name
-    expect(page.locator('#e2e_header_isLogined')).not_to_be_attached()
+    # screenshot_count = 1
+    # name = inspect.currentframe().f_code.co_name
+    # expect(page.locator('#e2e_header_isLogined')).not_to_be_attached()
     page.goto("https://openmind.test.osinfra.cn/")
-    shot(page, name, screenshot_count)
-    screenshot_count += 1
+    # shot(page, name, screenshot_count)
+    # screenshot_count += 1
     page.wait_for_selector('#e2e_login_account')
     page.locator('#e2e_login_account').fill('18801113053')
-    shot(page, name, screenshot_count)
-    screenshot_count += 1
+    # shot(page, name, screenshot_count)
+    # screenshot_count += 1
     page.locator('#e2e_login_password').fill('66liuyu!')
-    shot(page, name, screenshot_count)
-    screenshot_count += 1
+    # shot(page, name, screenshot_count)
+    # screenshot_count += 1
     page.click('#e2e_login_submit')
-    while True:
-        if page.get_by_text('登录成功'):
-            time.sleep(1.5)
-            shot(page, name, screenshot_count)
-            break
-    expect(page.locator('#e2e_header_isLogined')).to_be_attached()
+    page.wait_for_timeout(2000)
+    if not page.locator('#e2e_header_isLogined'):
+        login_sh(page)
+
 
 
 def hover_user_img(page: sync_api.Page):
     screenshot_count = 1
     name = inspect.currentframe().f_code.co_name
     page.hover('#e2e_header_isLogined')
-    page.wait_for_timeout(500)
-    shot(page, name, screenshot_count)
+    page.wait_for_timeout(200)
+    # shot(page, name, screenshot_count)
 
 
 def hover_create_organization(page: sync_api.Page, arglist: list):
     screenshot_count = 1
     name = inspect.currentframe().f_code.co_name
     page.locator('#e2e_headerUser_newOrg').hover()
-    shot(page, name, screenshot_count)
+    # shot(page, name, screenshot_count)
     page.locator('#e2e_headerUser_newOrg').click()
-    shot(page, name, screenshot_count)
+    # shot(page, name, screenshot_count)
     page.locator('#e2e_newOrg_nameInput').fill(arglist[0])
     page.locator('#e2e_newOrg_fullnameInput').fill(arglist[1])
     page.locator('.o-upload-input').all()[0].set_input_files('favicon.png')
     page.locator('.o-textarea-textarea').fill(arglist[3])
     page.get_by_placeholder("关联组织官网或GitHub地址").fill(arglist[4])
-    shot(page, name, screenshot_count)
+    # shot(page, name, screenshot_count)
     page.locator('#e2e_newOrg_submitBtn').click()
+    page.wait_for_timeout(500)
 
 
 def hover_create_model(page: sync_api.Page, arglist: list):
-    name = inspect.currentframe().f_code.co_name
+    # name = inspect.currentframe().f_code.co_name
     page.locator('#e2e_headerUser_newModel').hover()
     page.locator('#e2e_headerUser_newModel').click()
     # 填入参数
@@ -84,6 +81,7 @@ def hover_create_model(page: sync_api.Page, arglist: list):
     page.locator('#e2e_newModels_licenseSelect').click()
     page.get_by_text(arglist[3], exact=True).click()
     page.locator('#e2e_newModels_submitBtn').click()
+    page.wait_for_timeout(500)
     # input()
 
 
@@ -92,24 +90,26 @@ def enter_user_center(page: sync_api.Page):
     # 进入个人中心
     page.locator('#e2e_headerUser_center').hover()
     page.locator('#e2e_headerUser_center').click()
+    page.wait_for_timeout(500)
 
 
 def show_my_models(page: sync_api.Page):
     name = inspect.currentframe().f_code.co_name
     page.locator('#e2e_myAside_creation').click()
     page.get_by_text('我的模型').click()
+    page.wait_for_timeout(500)
 
 
 def show_point_model(page: sync_api.Page, arglist: list):
     print(arglist)
     name = inspect.currentframe().f_code.co_name
     page.locator('.title').get_by_text(arglist[0]).click()
-    time.sleep(1.5)
+    page.wait_for_timeout(500)
 
 
 def delete_current_model(page: sync_api.Page):
     name = inspect.currentframe().f_code.co_name
-    page.get_by_text('设置').click()
+    page.locator('.o-tab-nav').get_by_text('设置').click()
     page.locator('#e2e_deleteModel_inputValidation').fill('我已知晓，并确认删除模型')
     page.locator('#e2e_deleteModel_deleteBtn').click()
 
@@ -118,23 +118,26 @@ def logout(page: sync_api.Page):
     name = inspect.currentframe().f_code.co_name
     hover_user_img(page)
     page.get_by_text('退出登录').click()
+    page.wait_for_timeout(1500)
 
 
 def edit_model_tags(page: sync_api.Page, arglist: list):
-    pass
     name = inspect.currentframe().f_code.co_name
     arglist = tag_list_operation(arglist)
+    expect(page.locator('.o-btn.o-btn-primary.o-btn-large.o-btn-solid.use-openmind')).not_to_be_attached()
     page.get_by_text('编辑标签').click()
     for arg in arglist:
-        if type(arg) == list:
+        if type(arg) is list:
             page.get_by_placeholder(tag_dict[arg[0]]).click()
-            page.wait_for_timeout(500)
+            page.wait_for_timeout(200)
             for index in range(len(arg[1])):
                 page.wait_for_timeout(200)
                 page.locator('.o-option').get_by_text(arg[1][index], exact=True).click()
     page.get_by_text('提交').click()
     page.get_by_placeholder('Update README.md').fill(arglist[-1])
     page.locator('.o-btn.o-btn-primary.o-btn-large.o-btn-solid.o-dlg-btn').get_by_text('提交').click()
+    page.wait_for_timeout(200)
+
 
 
 def create_file(page: sync_api.Page, arglist: list):
@@ -148,6 +151,18 @@ def create_file(page: sync_api.Page, arglist: list):
     page.locator('.o-btn.o-btn-primary.o-btn-large.o-btn-solid.upload').get_by_text('提交').click()
     page.locator('.o-textarea-textarea').fill(arglist[2])
     page.locator('.o-btn.o-btn-primary.o-btn-large.o-btn-solid.o-dlg-btn').get_by_text('提交').click()
+    page.wait_for_timeout(500)
+
+
+def delete_file(page: sync_api.Page, arglist: list):
+    name = inspect.currentframe().f_code.co_name
+    page.locator('.o-tab-nav').get_by_text('文件').click()
+    page.get_by_title(arglist[0]).click()
+    # page.locator('.hover-stress').get_by_title(arglist[0]).click()
+    page.locator('.hover-stress').get_by_text('删除').click()
+    page.wait_for_timeout(500)
+    page.locator('.o-btn.o-btn-primary.o-btn-large.o-btn-solid.o-dlg-btn').get_by_text('删除').click()
+    page.wait_for_timeout(500)
 
 
 def_dict = {'登录sh环境': login_sh,
@@ -160,7 +175,8 @@ def_dict = {'登录sh环境': login_sh,
             '删除当前模型': delete_current_model,
             '退出登录': logout,
             '编辑模型标签': edit_model_tags,
-            '新建文件': create_file
+            '新建文件': create_file,
+            '删除文件': delete_file,
             }
 tag_dict = {'pipeline_tag': '新增标签',
             'frameworks': '新增框架',
